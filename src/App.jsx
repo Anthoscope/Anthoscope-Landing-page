42dab71015a34dc988e1c2d2491e5cfcb8a4ccdd
Epatsatzaki <evapatsatzakis@gmail.com>
2026-04-08 00:00:43 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 
 function App() {
+  // If we are in development mode, use "/"
+  // If we are in production (GitHub), use "/Anthoscope-Landing-page"
+  const baseName = import.meta.env.DEV ? '/' : '/Anthoscope-Landing-page';
+
   return (
-    <Router>
+    <Router basename={baseName}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
