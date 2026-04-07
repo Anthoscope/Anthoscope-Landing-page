9fe41552b315b69abe87c60a9328d9fb2358dffe
Epatsatzaki <evapatsatzakis@gmail.com>
2026-04-07 23:50:14 +0300

    hopefully works

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: './', 
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // If we are in 'serve' (npm run dev), use root '/'
+    // If we are in 'build' (GitHub Actions), use the repo folder
+    base: command === 'serve' ? '/' : '/Anthoscope-Landing-page/',
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    }
+  }
 })
