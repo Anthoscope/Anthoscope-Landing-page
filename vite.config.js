b5d608931f2306f4d0be89cf4e8f5bf24fb76783
Epatsatzaki <evapatsatzakis@gmail.com>
2026-04-07 23:21:48 +0300

    test2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/Anthoscope-Landing-page/', 
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // If we are running 'npm run dev', use root '/'
+    // If we are building for GitHub, use the repo name
+    base: command === 'serve' ? '/' : '/Anthoscope-Landing-page/',
+  }
 })
