16ff565282e7676b8bc603512eae07fd2205acc3
Epatsatzaki <evapatsatzakis@gmail.com>
2026-04-07 23:40:36 +0300

    aaaaaaaaa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // This makes all paths relative, which is safest for GitHub Organizations
   base: './', 
 })
